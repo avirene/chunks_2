1ba0ab81fbbc56ae86d95b2bdb4bbfe940ee1562
Iryna Audzei <avirene@live.com>
2021-05-04 13:58:01 +0100

    Updated naming

---
+++ src/entry-service.js
@@ -0,0 +1,29 @@
+import { nanoid } from 'nanoid';
+
+let entries = [
+  { id: nanoid(), word: 'React', def: 'UI Library'},
+  { id: nanoid(), word: 'Pizza', def: 'A tasty Italian food'},
+];
+
+const addEntry = ({ word, def }) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const newEntry = { id: nanoid(), word, def };
+      entries = [ ...entries, newEntry];
+      resolve(newEntry);
+    }, 500);
+  });
+};
+
+const getEntries = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(entries);
+    }, 1500);
+  });
+};
+
+export {
+  addEntry,
+  getEntries
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { addEntry, getEntries } from './entry-service';
+
+
+const App = () => {
+  return <HomePage />;
+}
+
+const HomePage = () => {
+
+  const [entries,  setEntries] = useState([]);
+
+  useEffect(() => {
+    // loading
+    getEntries()
+      .then((entries) => {
+        setEntries(entries)
+      })
+      .catch(() => {
+        // handle error
+      })
+  }, []);
+
+  const handleAddedEntry = (entry) => {
+    setEntries((oldEntries) => [entry, ...oldEntries]);
+  };
+
+  return (
+    <div>
+      <NewEntryContainer onEntryAdded={handleAddedEntry}/>
+      <EntryList entries={entries}/>
+    </div>
+  );
+};
+
+// logic and data access
+const NewEntryContainer = ({ onEntryAdded }) => {
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleNewEntry = (entry) => {
+    setLoading(true);
+    addEntry(entry)
+      .then((res) => {
+        setLoading(false);
+        onEntryAdded(res);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err);
+      })
+  }
+
+  return (
+    <NewEntryForm onNewEntry={handleNewEntry} loading={loading} error={error}/>
+  );
+}
+
+// Presentation only
+const NewEntryForm = ({ onNewEntry, loading, error }) => {
+
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      onNewEntry({ word: 'Hello', def: 'World' })
+    }}>
+      { loading && <div>loading</div> }
+      { error && <div>error</div> }
+      Term <input name="word"/>
+      Def <input name="def"/>
+      <button>Submit</button>
+    </form>
+  );
+}
+
+const EntryList = ({ entries }) => {
+  const elements = entries.map(({ id, word, def }) => {
+    return(
+      <React.Fragment key={id}>
+        <dt>{ word }</dt>
+        <dd>{ def }</dd>
+      </React.Fragment>
+    )
+  });
+
+  return (
+    <dl>
+      { elements }
+    </dl>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +99,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
